6bbe6e652a1689fa43a36d0b16ed84ccb606d32e
Unknown <dlomugdang@mail.csuchico.edu>
2017-04-25 12:03:43 -0700

    do/while loop for shuffle gallery

--- gallery.js
+++ gallery.js
@@ -1,6 +1,16 @@
-
+var chico = ["Bear Hole", "Monkey Face", "Table Mountain", "Feather Falls", "Chico State University"];
+var current;
 
 function shuffleGallery()
 {
+  var element;// = chico[Math.floor(Math.random() * chico.length)];
+  //console.log(element);
 
+  do
+  {
+    element = chico[Math.floor(Math.random() * chico.length)];
+  }
+  while (element == current);
+  console.log(element);
+  current = element;
 }
