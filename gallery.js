4af66a71785410fe943f4f6df87660fee97f493d
Unknown <dlomugdang@mail.csuchico.edu>
2017-04-27 11:22:15 -0700

    Hopefully random generator of pics

--- gallery.js
+++ gallery.js
@@ -1,4 +1,28 @@
-var chico = ["Bear Hole", "Monkey Face", "Table Mountain", "Feather Falls", "Chico State University"];
+/*var chico = ["Bear Hole", "Monkey Face",
+"Table Mountain", "Feather Falls",
+ "Chico State University"];*/
+var chico = [
+  { description: "Bear Hole",
+    picture: "https://s-media-cache-ak0.pinimg.com/736x/91/51/cd/9151cdacefa31f37bbd2fdcbbef89f1b.jpg",
+    from: "pinterest.com"
+  },
+  { description: "Monkey Face",
+    picture: "http://image.chicoer.com/storyimage/NA/20160113/NEWS/160119920/AR/0/AR-160119920.jpg&maxh=400&maxw=667",
+    from: "chicoer.com"
+  },
+  { description: "Table Mountain",
+    picture: "https://www.outdoorproject.com/sites/default/files/styles/odp_header_adaptive/public/features/img_2573_0.jpg?itok=j_s8RVKF",
+    from: "outdoorproject.com"
+  },
+  { description: "Feather Falls",
+    picture: "https://media-cdn.tripadvisor.com/media/photo-s/0e/aa/ef/cd/feather-falls-from-the.jpg",
+    from: "tripadvisor.com"
+  },
+  { description: "Chico State University",
+    picture: "http://www.csuchico.edu/cob/images/kendall-hall.jpg",
+    from: "csuchico.edu"
+  },
+]
 var current;
 
 function shuffleGallery()
